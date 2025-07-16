3ffc2f4acf6d2268c0a889513bfb9003aef65234
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-07-16 22:49:49 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Canvas } from './components/Canvas';
+
+function App() {
+    return (
+        <>
+            <div className="pencil-canvas canvas w-full min-h-screen">
+                <Canvas />
+            </div>
+        </>
+    );
+}
+export default App;
