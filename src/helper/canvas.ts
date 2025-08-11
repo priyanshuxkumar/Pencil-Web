0522a99dda829c1bd09d22cb5d1806eec2a10fed
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-08-11 16:26:59 +0530

    feat: add function to calculate bounding box for shapes

--- src/helper/canvas.ts
+++ src/helper/canvas.ts
@@ -1,3 +1,5 @@
+import { AvailableTools, type Shape } from '../types';
+
 export function normalizeRect(x1: number, y1: number, x2: number, y2: number) {
     const x = Math.min(x1, x2);
     const y = Math.min(y1, y2);
@@ -16,4 +18,34 @@ export function getRoomIdAndKey(): { roomId: string; roomKey: string } {
         roomId,
         roomKey,
     };
+}
+
+export function getBoundingBox(shape: Shape): { x: number; y: number; width: number; height: number } {
+    switch (shape.type) {
+        case AvailableTools.Rectangle:
+        case AvailableTools.Diamond:
+            return {
+                x: shape.x,
+                y: shape.y,
+                width: shape.width,
+                height: shape.height,
+            };
+        case AvailableTools.Ellipse:
+            return {
+                x: shape.x,
+                y: shape.y,
+                width: shape.radiusX * 2,
+                height: shape.radiusY * 2,
+            };
+        case AvailableTools.Line:
+        case AvailableTools.Arrow: {
+            const x = Math.min(shape.x, shape.x + shape.dx);
+            const y = Math.min(shape.y, shape.y + shape.dy);
+            const width = Math.abs(shape.dx);
+            const height = Math.abs(shape.dy);
+            return { x, y, width, height };
+        }
+        default:
+            return { x: 0, y: 0, width: 0, height: 0 };
+    }
 }
