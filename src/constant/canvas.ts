f8b016337c9c4cc42ad2018a823e5799a7ae2f5e
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-08-13 22:16:13 +0530

    feat: add constant

---
+++ src/constant/canvas.ts
@@ -0,0 +1,14 @@
+const STROKE_COLORS = ['#000', '#f87171', '#22c55e', '#60a5fa', '#f97316', '#d1d5db'];
+const BACKGROUND_COLORS = ['transparent', '#1f2937', '#7f1d1d', '#166534', '#1d4ed8', '#a16207', '#4b5563'];
+const STROKE_WIDTHS = [2, 3, 5];
+const STROKE_STYLES = [0, 3];
+const EDGES = [0, 15];
+
+
+export {
+    STROKE_COLORS,
+    BACKGROUND_COLORS,
+    STROKE_WIDTHS,
+    STROKE_STYLES,
+    EDGES
+}
