a3b18fe66a3b35063a681ca305cc65b74a4cef88
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-08-13 21:10:19 +0530

    feat: add types for WebSocket server events

--- src/types/socket.types.ts
+++ src/types/socket.types.ts
@@ -1,68 +1,25 @@
-import type { Shape } from "./shapes.types";
+import type { RoomUser } from '../components/Canvas';
+import type { Shape } from './shapes.types';
 
-type JoinRoom = {
-    type: 'join-room';
-    payload: {
-        roomId?: string;
-        userId?: string;
-    };
-};
-
-type UserJoin = {
-    type: 'join';
-    payload: {
-        roomId?: string;
-        shapes?: Shape[];
-    };
-};
-
-type ShapeCreated = {
-    type: 'create-shape';
-    payload: {
-        shape?: Shape;
-        roomId?: string;
-    };
-};
-
-type RemoveShape = {
-    type: 'remove-shape';
-    payload: {
-        shapeId?: string;
-        roomId?: string;
-    };
-};
-
-type CursorPosition = {
-    type: 'cursor-position';
-    payload: {
-        roomId?: string;
-        x?: number;
-        y?: number;
-    };
-};
-
-type SelectShape = {
-    type: 'select-shape';
-    payload: {
-        roomId: string;
-        shapeId?: string;
-    };
-};
-
-type ShapeResize = {
-    type: 'resize-shape';
-    payload: {
-        roomId?: string;
-        shape?: Shape;
-    };
-};
-
-
+// Send event types
 export type SocketMessage =
-    | JoinRoom
-    | UserJoin
-    | ShapeCreated
-    | RemoveShape
-    | CursorPosition
-    | SelectShape
-    | ShapeResize;
+    | { type: 'join-room'; payload: { roomId: string; userId: string } }
+    | { type: 'join'; payload: { roomId: string; shapes?: Shape[] } }
+    | { type: 'create-shape'; payload: { shape: Shape; roomId: string } }
+    | { type: 'remove-shape'; payload: { shapeId: string; roomId: string } }
+    | { type: 'cursor-position'; payload: { roomId: string; x: number; y: number } }
+    | { type: 'select-shape'; payload: { roomId: string; shapeId?: string } }
+    | { type: 'resize-shape'; payload: { roomId: string; shape: Shape } };
+
+// Server events
+export type ServerSocketEvent =
+    | { type: 'joined'; data: { name: string; userId: string; roomId: string }; message: string }
+    | { type: 'room-joined'; data: { name: string; userId: string; roomId: string; shapes: Shape[]; existingUsers: RoomUser[] }}
+    | { type: 'user-room-joined'; data: RoomUser }
+    | { type: 'shape-created'; data: { shape: Shape; name: string } }
+    | { type: 'shape-removed'; data: { shapeId: string; name: string } }
+    | { type: 'cursor-position'; data: { id: string; name: string; roomId: string; x: number; y: number } }
+    | { type: 'selected-shape'; data: { shapeId: string } }
+    | { type: 'resized-shape'; data: { updatedShape: Shape } }
+    | { type: 'user-exit'; data: { userId: string; roomId: string } }
+    | { type: 'error'; message: string };
